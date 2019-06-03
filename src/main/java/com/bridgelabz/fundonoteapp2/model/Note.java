77fae6c114d89f4ca51557b753a721a696dbd716
Ranjithamshivamatta <ranju0496@gmail.com>
2019-06-03 18:57:16 +0530

    03-06-2019

--- src/main/java/com/bridgelabz/fundonoteapp2/model/Note.java
+++ src/main/java/com/bridgelabz/fundonoteapp2/model/Note.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -12,6 +14,7 @@ import javax.persistence.Table;
 public class Note {
 
 	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private int noteId;
 	private String title;
 	private String discription;
